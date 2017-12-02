c06ebd57fc6035e642476c8db9a49db72aceab99
Satya <spatra00@gmail.com>
2017-12-02 16:12:07 +0530

    code refracted and routes added

--- server/server.js
+++ server/server.js
@@ -1,28 +1,28 @@
-var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var {mongoose} = require('./db/mongoose.js')
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
-var Todo = mongoose.model('Todo', {
-  text: {
-    type: String
-  },
-  completed: {
-    type: Boolean
-  },
-  completedAt:{
-    type: Number
-  }
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos',(req,res) =>{
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc)=>{
+    res.send(doc);
+  }, (e) =>{
+    res.status(400).send(e);
+  })
 });
 
-var newTodo = new Todo({
-  text: 'Watch movie' ,
-  completed: true ,
-  completedAt: 1/12/2017
-});
 
-newTodo.save().then((doc)=>{
-  console.log('Saved todo', doc);
-}, (e) =>{
-  console.log('Unabble to save todo');
+
+app.listen(3000, () =>{
+  console.log('Started on 3000');
 });
