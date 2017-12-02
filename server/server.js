867b099018d734ad3d9a2fa0500193a4ed535fd6
Satya <spatra00@gmail.com>
2017-12-03 02:11:12 +0530

    Deploying

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ var {User} = require('./models/user');
 
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -55,7 +56,7 @@ app.get('/todos/:id', (req, res) =>{
 
 })
 
-app.listen(3000, () =>{
+app.listen(PORT, () =>{
   console.log('Started on 3000');
 });
 
