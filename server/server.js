37a783d9904d51e20934c6f680964115322af0fb
Satya <spatra00@gmail.com>
2017-12-03 13:47:50 +0530

    Done with it

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectId} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose.js')
@@ -74,11 +75,35 @@ app.delete('/todos/:id', (req, res) =>{
       }
     }, (e) =>{
       res.status(400).send(e);
+    }).catch((e)=>{
+      res.status(400).send();
     });
   }
+});
 
+app.patch('/todos/:id', (req,res) =>{
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+  if(!ObjectId.isValid(id)){
+    return res.status(404).send();
+  }
 
-})
+  if(_.isBoolean(body.completed) && body.completed){
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body},{new: true}).then((todo)=> {
+    if(!todo){
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e)=>{
+    res.status(400).send
+  })
+});
 
 app.listen(3000, () =>{
   console.log('Started on 3000');
